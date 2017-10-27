74f9b522c07f453f268340920f1260f6f03019c5
DamianVogel <rodolfovogel@gmail.com>
2017-10-26 21:31:43 -0300

    Arduino Pulsador Funcionando
    .

---
+++ Arduino_Pulsador_2017/Arduino_Pulsador.ino
@@ -0,0 +1,33 @@
+void setup() {
+   pinMode(7,INPUT);
+    
+   pinMode(13,OUTPUT);
+  Serial.begin(9600);
+  
+}
+
+int boton=0;
+int estadoLed=0;
+
+int botonAnterior = 0;
+
+void loop() 
+{
+       boton=digitalRead(7);
+
+      //Serial.println(estadoLed);
+     
+       if(boton==1 && botonAnterior == 0)
+       {
+          estadoLed=1-estadoLed;
+          digitalWrite(13,estadoLed);
+          delay(500);
+                
+       }
+
+        botonAnterior = boton;
+       
+
+
+}
+
